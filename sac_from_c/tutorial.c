fddb2f9ec699603f3a30c25af31c5574261808f6
Sven-Bodo Scholz <sbs@sac-home.org>
2007-10-15 13:18:14 +0000

    adjusted tutorial.c

--- sac_from_c/tutorial.c
+++ sac_from_c/tutorial.c
@@ -1,8 +1,9 @@
 /********  c program using a SAC module exported as a c library *******/
+
 #include <stdlib.h>
 #include <stdio.h>
-#include "sac_cinterface.h"
-#include "tutorialMod.h"
+#include "sacinterface.h"
+#include "include/tutorialMod_c.h"
 
 #define SMALLARRAY 20
 
@@ -12,49 +13,64 @@ int main()
   int *int_data_in;
   int *int_data_out;
 
-  SAC_arg vector_out;
-  SAC_arg vector_in;
-  SAC_arg parameter;
+  SACarg vector_out;
+  SACarg vector_in;
+  SACarg parameter;
 
-  SAC_InitRuntimeSystem();
-  
-  /* init data */
+  /**
+   *  init C data vector:
+   */
   int_data_in=(int*)malloc(SMALLARRAY*sizeof(int));
-  for(i=0; i<SMALLARRAY; i++) {int_data_in[i]=i+1;}
-
-  /* convert c data to abstract datatype SAC_arg */
-  vector_in = SAC_IntArray2Sac(SAC_CONSUME_ARG, int_data_in, 1, SMALLARRAY);
-  parameter = SAC_Int2Sac(99);
-
-  /* call to SAC-function */
-  if(SAC_tutorialMod_addValue_1_2(&vector_out, vector_in, parameter)) {
-    printf("some error occured, calling the function!\n\n");
-    exit(1);
+  for(i=0; i<SMALLARRAY; i++) {
+    int_data_in[i]=i+1;
   }
 
-  /* reuse arg for next function call */
-  vector_in = vector_out;
+  /**
+   * convert c data to abstract datatype SAC_arg:
+   */
+  vector_in = SACARGconvertFromIntPointer( int_data_in, 1, SMALLARRAY);
+  parameter = SACARGconvertFromInt(99);
 
-  /* increase refcounter to use argument twice */
-  SAC_SetRefcounter(vector_in, 2);
+  /**
+   * Now, we call a SAC-function. Note here, that this consumes
+   * both our SACargs. As a consequence, any reference to int_data_in
+   * is illegal!! In fact, the memory of int_data_in might have been
+   * released or reused!!
+   */
+  tutorialMod__addValue2( & vector_out,
+                          vector_in,
+                          parameter);
 
-  /* call to SAC-function */
-  if(SAC_tutorialMod_addVectors_1_2(&vector_out, vector_in, vector_in)) {
-    printf("some error occured, calling the function!\n\n");
-    exit(1);
-  }
+  /**
+   * Now, we use the result of the last SaC function call as argument
+   * for another SaC function call. However, since we want to use the
+   * same SACarg twice, we have to use SACARGnewReference() in one of
+   * the argument positions.
+   */
+  tutorialMod__addVectors2( & vector_out,
+                            SACARGnewReference( vector_out),
+                            vector_out);
+
   
-  /* convert and consume the result */
-  int_data_out = SAC_Sac2IntArray(SAC_CONSUME_ARG, vector_out);
+  /**
+   * Finally, we want to "transfer" the result of the last call back
+   * into the C world. Note again, that this is a consuming operation,
+   * i.e., the SACarg vector_out is being freed implicitly here.
+   * Instead, we obtain int_data_out whose memory management is now
+   * entirely in our control.
+   */
+  int_data_out = SACARGconvertToIntArray( vector_out);
   
-  /* print result */
-  /* print elements of c array */
+  /**
+   * Print the elements of the c array
+   * and free the memory associated to the pointer
+   */
   for(i=0; i<SMALLARRAY; i++)	
     printf("%d ", int_data_out[i]);
   printf("\n");
 
   free(int_data_out); 
-  SAC_FreeRuntimeSystem();
+
   return(0);
 }
 
